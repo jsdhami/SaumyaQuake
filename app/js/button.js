f27b490e10bef321f619db0245333649413cb1ef
Janak Singh Dhami <139155991+jsdhami@users.noreply.github.com>
2023-10-08 05:36:07 +0545

    go

---
+++ app/js/button.js
@@ -0,0 +1,22 @@
+   // Get references to the button and popup container for left graph show
+   const togglePopupButton = document.getElementById('toggle-popup');
+   const popup = document.getElementById('popup');
+
+   // Function to toggle the popup visibility
+   function togglePopup() {
+    
+       if (popup.style.display === 'none' || popup.style.display === '') {
+           popup.style.display = 'block';
+       } else {
+           popup.style.display = 'none';
+       }
+   }
+
+   // Event listener for the toggle button
+   togglePopupButton.addEventListener('click', togglePopup);
+
+
+
+
+//    graph show
+
