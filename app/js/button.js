714d42c26b5da02f508d38be8f34f08ecd23e0f0
Janak singh dhami <info@jsdhami.com.np>
2023-10-08 07:20:53 +0545

    error handling

--- app/js/button.js
+++ app/js/button.js
@@ -1,15 +1,15 @@
  // Get references to the button and popup container
- const togglePopupButton = document.getElementById('toggle-popup');
- const popup = document.getElementById('popup');
+        const togglePopupButton = document.getElementById('toggle-popupjj');
+        const popup = document.getElementById('popupforqd');
 
- // Function to toggle the popup visibility
- function togglePopup() {
-     if (popup.style.display === 'none' || popup.style.display === '') {
-         popup.style.display = 'block';
-     } else {
-         popup.style.display = 'none';
-     }
- }
+        // Function to toggle the popup visibility
+        function togglePopup() {
+            if (popup.style.display === 'none' || popup.style.display === '') {
+                popup.style.display = 'block';
+            } else {
+                popup.style.display = 'none';
+            }
+        }
 
- // Event listener for the toggle button
- togglePopupButton.addEventListener('click', togglePopup);
+        // Event listener for the toggle button
+        togglePopupButton.addEventListener('click', togglePopup);
